dde223d857869d4c3c56a097df9486db528d023a
Clayton Louden <clouden@student.42madrid.com>
2025-06-03 20:50:41 +0200

    general outline of get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: clouden <clouden@student.42madrid.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 13:01:42 by clouden           #+#    #+#             */
-/*   Updated: 2025/06/03 14:41:40 by clouden          ###   ########.fr       */
+/*   Updated: 2025/06/03 20:49:42 by clouden          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,63 @@
 #include <stdio.h>
 #include <fcntl.h>
 
+int ft_lengto(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str != '\n' && *str != '\0')
+	{
+		len++;
+		str++;
+	}
+	if (*str == '\n')
+		return (++len);
+	return (len);
+}
+
 char *get_next_line(int fd)
 {
-	static char *buf;
-
-	buf = malloc(BUFFER_SIZE);
-    if (buf == NULL)
+	static char	*buff;
+	int			len;
+	char 		*newln;
+	int			i;
+	
+	i = 0;
+	if (BUFFER_SIZE <= 0)
+		return (NULL);
+	buff = malloc(BUFFER_SIZE + 1);	
+    if (buff == NULL)
         return (NULL);
-	s = read(fd, buf, BUFFER_SIZE);
-
+	read(fd, buff, BUFFER_SIZE);
+	len = ft_lengto(buff);
+	newln = malloc(len + 1);
+	while (*buff != '\0')
+	{
+		newln[i] = *buff;
+		if (newln[i] != '\n')
+		{
+			i = 0;
+			buff++;
+			return (newln);
+		}
+		i++;
+	}
+	if (*buff == '\0')
+	{
+		remeasure your newln
+		fill new memory allocation with current new line
+		continue filling new line with buffer
+	}
 }
 
 int main(void)
 {
     int     fd;
-    char    *buffer;
-    int     size;
-    size_t  bytes;
+	char	*newln;
     
     fd = open("titus.txt", O_RDONLY);
-    size = 10;
-    buffer = malloc(size);
-    buffer[size] = 0;
-    bytes = read(fd, buffer, size - 1);
-    printf("%zu\n",bytes);
-    printf("%s\n", buffer);
+    newln = get_next_line(fd);
     
     bytes = read(fd, buffer, size - 1);
     printf("%zu\n",bytes);
